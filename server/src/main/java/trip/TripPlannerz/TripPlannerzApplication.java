79c820d014b6449bec9ab9457b4f765c01d14470
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-12-17 10:19:18 +0900

    feat: configure main class

---
+++ server/src/main/java/trip/TripPlannerz/TripPlannerzApplication.java
@@ -0,0 +1,11 @@
+package trip.TripPlannerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlannerzApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TripPlannerzApplication.class, args);
+    }
+}
